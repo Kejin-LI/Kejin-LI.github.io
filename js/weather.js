90cec52cbe7aa81fc3920fd1b29dfe9875b9dd0d
Kejin-LI <likejin2019@gmail.com>
2022-12-12 00:45:58 +0800

    Site updated: 2022-12-12 00:45:58

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "FCAEAE",
+    "tmpSize": "16",
+    "cityColor": "FCAEAE",
+    "citySize": "16",
+    "aqiColor": "FCAEAE",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "true",
+    "vertical": "top",
+    "horizontal": "left",
+    "left": "150",
+    "top": "10",
+    "key": "a8b44ee35b2e4955a21437eb307e4cea"
+  }
+}

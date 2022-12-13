3039f07d66261701f4d7a9f62f6573d1ee0cfa7c
Kejin-LI <likejin2019@gmail.com>
2022-12-14 00:25:23 +0800

    Site updated: 2022-12-14 00:25:23

--- js/crash_cheat.js
+++ js/crash_cheat.js
@@ -3,12 +3,12 @@
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/img/profile.png");
-         document.title = '你的身份证落我这了🙈~';
+         document.title = '⚠️对方正在挖掘你的秘密🙈...';
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/img/profile.png");
-         document.title = ' 哈哈上当了吧( ๑•̀ㅂ•́) ✧';
+         document.title = '哈哈上当了吧( ๑•̀ㅂ•́) ✧';
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
